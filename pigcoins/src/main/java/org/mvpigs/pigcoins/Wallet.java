02b674ec2f41412310e42a2f67dc6f8c0467a1b1
Adrián Ponce Cano <adrianponcecano@gmail.com>
2018-02-20 14:23:51 +0100

    clase Wallet

--- pigcoins/src/main/java/org/mvpigs/pigcoins/Wallet.java
+++ pigcoins/src/main/java/org/mvpigs/pigcoins/Wallet.java
@@ -1,1 +1,19 @@
 package org.mvpigs.pigcoins;
+
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Wallet {
+    private PublicKey address;
+    private PrivateKey SK;
+    private double totalInput = 0d;
+    private double totalOutput = 0d;
+    private double balance = 0d;
+    private ArrayList<Transaction> inputTransactions = new ArrayList();
+    private ArrayList<Transaction> outputTransactions = new ArrayList();
+}
+
