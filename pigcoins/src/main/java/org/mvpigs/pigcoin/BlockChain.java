c3a6b7983fa08b7af3a58a6fc5d703b943917c37
Adrián Ponce Cano <adrianponcecano@gmail.com>
2018-02-20 13:32:35 +0100

    Creada clase BlockChain

---
+++ pigcoins/src/main/java/org/mvpigs/pigcoin/BlockChain.java
@@ -0,0 +1,14 @@
+package org.mvpigs.pigcoins;
+
+import org.mvpigs.pigcoins.Transaction;
+
+import java.util.ArrayList;
+import java.security.PublicKey;
+import java.util.Map;
+
+
+public class BlockChain {
+
+    ArrayList<Transaction> blockchain = new ArrayList<Transaction>();
+
+}
---
+++ pigcoins/src/main/java/org/mvpigs/pigcoin/Transaction.java
@@ -0,0 +1,8 @@
+package org.mvpigs.pigcoin;
+
+import java.security.PublicKey;
+import java.util.ArrayList;
+
+public class BlockChain {
+
+    ArrayList<Transaction> blockchain = new ArrayList<Transaction>();
